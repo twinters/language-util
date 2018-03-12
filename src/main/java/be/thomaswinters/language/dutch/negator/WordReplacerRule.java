33332e3b3603d1bea4698b8629d8206a695c1fee
T. Winters <info@thomaswinters.be>
2018-03-12 01:09:01 +0100

    Made replacer rule use set instead of list

--- src/main/java/be/thomaswinters/language/dutch/negator/WordReplacerRule.java
+++ src/main/java/be/thomaswinters/language/dutch/negator/WordReplacerRule.java
@@ -1,27 +1,25 @@
 package be.thomaswinters.language.dutch.negator;
 
-import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import org.languagetool.AnalyzedTokenReadings;
 import org.languagetool.ILanguageTool;
 import org.languagetool.LanguageToolUtils;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 
 public class WordReplacerRule extends AbstractSentenceAnalysisRule {
 
     private final String searchWord;
     private final String replacementWord;
-    private final List<String> previousWordBlacklist;
+    private final Set<String> previousWordBlacklist;
 
-    public WordReplacerRule(ILanguageTool languageTool, String searchWord, String replacementWord, List<String> previousWordBlacklist) {
+    public WordReplacerRule(ILanguageTool languageTool, String searchWord, String replacementWord, Collection<? extends String> previousWordBlacklist) {
         super(languageTool);
         this.searchWord = searchWord;
         this.replacementWord = replacementWord;
-        this.previousWordBlacklist = ImmutableList.copyOf(previousWordBlacklist);
+        this.previousWordBlacklist = ImmutableSet.copyOf(previousWordBlacklist);
     }
 
     public WordReplacerRule(ILanguageTool languageTool, String searchWord, String replacementWord) {
