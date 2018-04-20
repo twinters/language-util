fad8f23527ff026a6f4722dc03334204a625e4e2
T. Winters <info@thomaswinters.be>
2018-04-21 01:13:54 +0200

    Removed some prints and used thread local random object.

--- src/main/java/be/thomaswinters/language/dutch/negator/AReplacerNegator.java
+++ src/main/java/be/thomaswinters/language/dutch/negator/AReplacerNegator.java
@@ -1,25 +1,23 @@
 package be.thomaswinters.language.dutch.negator;
 
-import be.thomaswinters.random.Picker;
 import be.thomaswinters.replacement.Replacer;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ThreadLocalRandom;
 
 public abstract class AReplacerNegator implements NegatorRule {
     @Override
     public Optional<String> negateAction(String input) throws IOException, ExecutionException {
         List<Replacer> possibleReplacers = getPossibleReplacers(input);
-        System.out.println(this + ", replacers: " + possibleReplacers);
-
         if (!possibleReplacers.isEmpty()) {
-            System.out.println("-- Possible antonyms: " + possibleReplacers);
-            Replacer chosenReplacer = Picker.pick(possibleReplacers);
+            System.out.println("-- Possible replacements: " + possibleReplacers);
+            Replacer chosenReplacer = possibleReplacers.get(
+                    ThreadLocalRandom.current().nextInt(possibleReplacers.size()));
             String output = chosenReplacer.replace(input);
             if (!output.equals(input)) {
-                System.out.println("-- Antonym");
                 return Optional.of(output);
             }
         }
