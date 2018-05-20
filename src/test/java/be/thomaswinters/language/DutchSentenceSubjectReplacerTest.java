48ddc96f1d3ae6a036fdef4835f17829f9f58f41
T. Winters <info@thomaswinters.be>
2018-05-20 23:44:38 +0200

    Removed subject replacer test

--- src/test/java/be/thomaswinters/language/DutchSentenceSubjectReplacerTest.java
+++ src/test/java/be/thomaswinters/language/DutchSentenceSubjectReplacerTest.java
@@ -1,16 +1,9 @@
 package be.thomaswinters.language;
 
 import be.thomaswinters.language.dutch.DutchSentenceSubjectReplacer;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
 import org.junit.Before;
-import org.junit.Test;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.List;
 
 public class DutchSentenceSubjectReplacerTest {
     private DutchSentenceSubjectReplacer replacer;
@@ -24,29 +17,29 @@ public class DutchSentenceSubjectReplacerTest {
         return replacer.replaceSecondPerson(input, "ik", "mijn", "mij", "mijzelf", SubjectType.FIRST_SINGULAR);
     }
 
-    @Test
-    public void WikiHow_Test() throws Exception {
-        File csvData = new File("data/wikihow.csv");
-        CSVParser parser = CSVParser.parse(csvData, Charset.defaultCharset(), CSVFormat.newFormat(';'));
-        List<CSVRecord> records = parser.getRecords();
-
-        int amountCorrect = 0;
-        int totalAmount = records.size();
-        for (CSVRecord csvRecord : records) {
-            String input = csvRecord.get(0);
-            String expectedOutput = csvRecord.get(1).toLowerCase();
-            String output = replaceDutchSecondToFirst(input).toLowerCase();
-
-            boolean right = false;
-            if (output.equals(expectedOutput)) {
-                amountCorrect += 1;
-                right = true;
-            }
-//            if (!right) {
-            System.out.println((right ? "Right!" : "WRONG!  " + input + "\nOutput: " + output + "\nExpctd: " + expectedOutput + "\n"));
+//    @Test
+//    public void WikiHow_Test() throws Exception {
+//        File csvData = new File("variations/wikihow.csv");
+//        CSVParser parser = CSVParser.parse(csvData, Charset.defaultCharset(), CSVFormat.newFormat(';'));
+//        List<CSVRecord> records = parser.getRecords();
+//
+//        int amountCorrect = 0;
+//        int totalAmount = records.size();
+//        for (CSVRecord csvRecord : records) {
+//            String input = csvRecord.get(0);
+//            String expectedOutput = csvRecord.get(1).toLowerCase();
+//            String output = replaceDutchSecondToFirst(input).toLowerCase();
+//
+//            boolean right = false;
+//            if (output.equals(expectedOutput)) {
+//                amountCorrect += 1;
+//                right = true;
 //            }
-
-        }
-        System.out.println("\nScore:" + amountCorrect + "/" + totalAmount);
-    }
+////            if (!right) {
+//            System.out.println((right ? "Right!" : "WRONG!  " + input + "\nOutput: " + output + "\nExpctd: " + expectedOutput + "\n"));
+////            }
+//
+//        }
+//        System.out.println("\nScore:" + amountCorrect + "/" + totalAmount);
+//    }
 }
