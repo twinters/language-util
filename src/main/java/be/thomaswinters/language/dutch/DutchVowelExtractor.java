a5c53ce1006dc7d71e1418fe51d3f89876b07b39
T. Winters <info@thomaswinters.be>
2018-07-07 19:06:35 +0200

    Fixed bug in DutchVowelExtractor

--- src/main/java/be/thomaswinters/language/dutch/DutchVowelExtractor.java
+++ src/main/java/be/thomaswinters/language/dutch/DutchVowelExtractor.java
@@ -1,18 +1,22 @@
 package be.thomaswinters.language.dutch;
 
-import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class DutchVowelExtractor {
 
     /**
      * Extracts (pairs of) vowels from the input
+     *
      * @param input
      * @return
      */
     public List<String> extractVowels(String input) {
-        return Arrays.asList(input.replaceAll("[aeiou]", " ").split(" "));
+        return Stream.of(
+                input.replaceAll("[^aeiou]", " ").split(" "))
+                .filter(e -> e.trim().length() > 0)
+                .collect(Collectors.toList());
     }
 
     /**
